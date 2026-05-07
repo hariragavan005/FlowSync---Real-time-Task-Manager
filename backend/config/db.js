1773187adcaa5ba3886d23320d42987e4f7e0096
Hari Haran G <hariragavan001@gmail.com>
2026-05-07 23:02:09 +0530

    Initial commit - FlowSync Task Manager

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'taskmanager'
+      dbName: 'Taskmanager'
     });
     console.log(`MongoDB Connected: ${conn.connection.host}, Database: ${conn.connection.name}`);
   } catch (error) {
